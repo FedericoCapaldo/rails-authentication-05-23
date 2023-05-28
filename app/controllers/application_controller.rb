8f14484b0c30d73bfb0ed348285f17d37c4b0e94
Federico <federico.capaldo@hotmail.com>
2023-05-28 15:35:30 +0100

    protecting all routes by default with authentication, whitelisting homepage to not need auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  # following line will make require login to all actions in all controllers by default
+  before_action :authenticate_user!
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
+  # will not require login
   def home
+  end
+
+  # will require login (by default)
+  def about
   end
 end
